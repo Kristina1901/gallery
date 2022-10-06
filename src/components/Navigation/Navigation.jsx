a54e35eee361fe41933797a950dfa5eafac09a42
Kristina Tuzova <ktuzova1901@gmail.com>
2022-10-07 00:29:06 +0300

    created component Nagigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import './Navigation.scss'
+const Navigation = () => (
+  <nav className='header'>
+    <NavLink
+    to="/"
+    end
+      className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+    >
+      Home
+    </NavLink>
+
+    <NavLink
+      to="/favorite"
+      className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+     
+    >
+      Favorite
+    </NavLink>
+    <hr />
+  </nav>
+);
+
+export default Navigation;
