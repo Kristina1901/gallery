9c4982ad11321a108b3ff70e8a660937eb6bf5cc
Kristina Tuzova <ktuzova1901@gmail.com>
2022-10-07 01:38:22 +0300

    react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,33 +2,27 @@ import { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './Navigation';
 import Loader from './Loader/Loader';
-import { useState} from 'react';
-const HomePage = lazy(() =>
-  import('./HomePage'),
-);
-const Favorite = lazy(() =>
-import('./Favorite'),
-);
+import { useState } from 'react';
+const HomePage = lazy(() => import('./HomePage'));
+const Favorite = lazy(() => import('./Favorite'));
 export const App = () => {
   const [array, setArray] = useState([]);
   const filterArray = (values, img) => {
-  let num = values.find(item => item.id === img);
-   setArray(prev => [...prev, num])
-   localStorage.setItem(`${img}`,JSON.stringify(num));
+    let num = values.find(item => item.id === img);
+    setArray(prev => [...prev, num]);
+    localStorage.setItem(`${img}`, JSON.stringify(num));
   };
   return (
     <>
-     <Navigation />
+      <Navigation />
 
-      <Suspense fallback={<Loader/>}>
-      <Routes>
-          <Route path="/" element={<HomePage filterArray={filterArray}/>} />
-          <Route path="/favorite" element={<Favorite array={array}/>} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage filterArray={filterArray} />} />
+          <Route path="/favorite" element={<Favorite array={array} />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
-     
     </>
-   
   );
 };
